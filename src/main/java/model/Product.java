9c45673339734d91e37cac4fb44a8bf13dce815b
hongquan16101995 <66354589+hongquan16101995@users.noreply.github.com>
2022-02-18 15:41:55 +0700

    CRUD cơ bản với Spring MVC

--- src/main/java/model/Product.java
+++ src/main/java/model/Product.java
@@ -1,19 +1,27 @@
 package model;
 
 public class Product {
+    private int id;
     private String name;
     private double price;
+    private String description;
 
     public Product() {
     }
 
-    public Product(String name) {
-        this.name = name;
-    }
-
-    public Product(String name, double price) {
+    public Product(int id, String name, double price, String description) {
+        this.id = id;
         this.name = name;
         this.price = price;
+        this.description = description;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
     }
 
     public String getName() {
@@ -30,5 +38,13 @@ public class Product {
 
     public void setPrice(double price) {
         this.price = price;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
     }
 }
